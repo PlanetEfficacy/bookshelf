bd4eaf35f7b196b591d7320260457aeaca419c34
PlanetEfficacy <jspevack@gmail.com>
2018-01-31 20:40:00 -0700

    Add book validations

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -11,5 +11,43 @@ RSpec.describe Book, type: :model do
   describe '#validations' do
     it { should belong_to :author }
     it { should belong_to :genre }
+    it { should validate_presence_of :author }
+    it { should validate_presence_of :genre }
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :published }
+    it { should validate_uniqueness_of :title }
+
+    describe 'amazon url and audible url' do
+      let(:amazon_url) { 'https://www.amazon.com' }
+      let(:audible_url) { 'https://www.audible.com' }
+
+      context 'when both are present' do
+        it 'is valid' do
+          subject = build :book, amazon_url: amazon_url, audible_url: audible_url
+          expect(subject).to be_valid
+        end
+      end
+
+      context 'when only amazon url is present' do
+        it 'is valid' do
+          subject = build :book, amazon_url: amazon_url, audible_url: nil
+          expect(subject).to be_valid
+        end
+      end
+
+      context 'when only audible url is present' do
+        it 'is valid' do
+          subject = build :book, amazon_url: nil, audible_url: audible_url
+          expect(subject).to be_valid
+        end
+      end
+
+      context 'when neither url is present' do
+        it 'is not valid' do
+          subject = build :book, amazon_url: nil, audible_url: nil
+          expect(subject).to be_invalid
+        end
+      end
+    end
   end
 end
